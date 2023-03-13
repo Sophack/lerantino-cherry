ca8dba2e87adbc906857ab35acd324a8239c84ab
Saraf Nawar <s.999895831@gmail.com>
2023-03-13 15:16:26 -0400

    testing commit

--- server.js
+++ server.js
@@ -25,12 +25,16 @@ const sess = {
   })
 };
 
+// serving static files 
 app.use(session(sess));
 
+// incorporate custom helper methods
 const hbs = exphbs.create({ helpers });
 
+// setting Handlebars.js as the default template engine 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,6 +42,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
+//start server to begin listening
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
   sequelize.sync({ force: false });
